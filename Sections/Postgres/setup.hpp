e0b322c3ec16cf84f5344d5f9834dda9499634f0
Safa Bayar <rectionxx@gmail.com>
2020-03-28 00:29:49 -0400

    updated Setup.hpp

--- Sections/Postgres/setup.hpp
+++ Sections/Postgres/setup.hpp
@@ -1,13 +1,19 @@
-void PostgresqlSetup(){
-  char * sql;
-  
-  try {
-    connection C("dbname = market user = postgres password = TEST hostaddr = 127.0.0.1 port = 5432");
-    if (C.is_open()) {
-      cout << "Opened database successfully: " << C.dbname() << endl;
-    } else {
-      cout << "Can't open database" << endl;
-      return 1;
+class PostgresSetup{
+  void PostgresqlSetup(){
+    char * sql;
+    
+    try {
+      connection C("dbname = market user = postgres password = TEST hostaddr = 127.0.0.1 port = 5432");
+      if (C.is_open()) {
+	cout << "Opened database successfully: " << C.dbname() << endl;
+      } else {
+	cout << "Can't open database" << endl;
+	return 1;
+      } catch (const std::exception &e) {
+	cerr << e.what() << std::endl;
+	return 1;
+      }
+      
     }
   }
-}
+};
--- Sections/Postgres/tablesetup.hpp
+++ Sections/Postgres/tablesetup.hpp
@@ -1,19 +1,22 @@
-void GamesTableInstallation(){
-  sql = "CREATE TABLE Market("				\
-    "ID INT PRIMARY KEY     NOT NULL,"			\
-    "PRODUCTNAME           CHAR(50)    NOT NULL,"	\
-    "ADDRESS        CHAR(50)    NOT NULL,"		\
-    "PRICE        INT     NOT NULL"			\
-    "PRODUCTYEAR            INT     NOT NULL);";
-  
-  work W(C);
-  
-  W.exec( sql );
-  W.commit();
-  cout << "Table created successfully" << endl;
-  C.disconnect ();
-} catch (const std::exception &e) {
-  cerr << e.what() << std::endl;
-  return 1;
- }
-}
+class GameTableInstall {
+  void GamesTableInstallation(){
+    try {
+      sql = "CREATE TABLE Market("			\
+	"ID INT PRIMARY KEY     NOT NULL,"		\
+	"PRODUCTNAME           CHAR(50)    NOT NULL,"	\
+	"ADDRESS        CHAR(50)    NOT NULL,"		\
+	"PRICE        INT     NOT NULL"			\
+	"PRODUCTYEAR            INT     NOT NULL);";
+      
+      work W(C);
+      
+      W.exec( sql );
+      W.commit();
+      cout << "Table created successfully" << endl;
+      C.disconnect ();
+    } catch (const std::exception &e) {
+      cerr << e.what() << std::endl;
+      return 1;
+    }
+  }
+};
