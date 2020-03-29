77d6c43de5d7cb6458e4e0d6c2428087b6e5ebe4
Safa Bayar <rectionxx@gmail.com>
2020-03-28 21:56:58 -0400

    Fixed All Errors and Warnings

--- Sections/Postgres/setup.hpp
+++ Sections/Postgres/setup.hpp
@@ -1,18 +1,21 @@
+#include <pqxx/pqxx> 
+
+using namespace pqxx;
+
 class PostgresSetup{
 public:
   int PostgresqlSetup(){
     try {
       connection C("dbname = market user = postgres password = TEST hostaddr = 127.0.0.1 port = 5432");
       if (C.is_open()) {
-	std::cout << "Opened database successfully: " << C.dbname() << endl;
+	std::cout << "Opened database successfully: " << C.dbname() << std::endl;
       } else {
-	std::cout << "Can't open database" << endl;
+	std::cout << "Can't open database" << std::endl;
 	return 1;
-      } catch (const std::exception &e) {
+      }} catch (const std::exception &e) {
 	std::cerr << e.what() << std::endl;
 	return 1;
       }
       return 0;
-    }
   }
 };
