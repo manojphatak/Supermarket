2cd46a10d333d822fd3fdb77b54fdec81475dd72
Safa Bayar <rectionxx@gmail.com>
2020-04-06 01:56:15 -0400

    Updated sections and tables

--- Sections/Postgres/setup.hpp
+++ Sections/Postgres/setup.hpp
@@ -5,18 +5,25 @@ using namespace pqxx;
 class PostgresSetup{
 public:
   int PostgresqlSetup(){
-    try {
-      connection C("dbname = market user = employee password = test hostaddr = 127.0.0.1 port = 5432");
-      if (C.is_open()) {
-	std::cout << "Opened database successfully: " << C.dbname() << std::endl;
-      } else {
-	std::cout << "Can't open database" << std::endl;
-	return 1;
-      }} catch (const std::exception &e) {
- std::cout << "DATABASE SETUP FAILED!";
-  std::cerr << e.what() << std::endl;
-	return 1;
-      }
-      return 0;
+	TableInstallForPostgres TableInstall;
+	if ( TableInstall.ExistTableTest() == 0 ){
+		if ( TableInstall.TableRecords() != 1 ){
+			std::cout << "The Tables Created Succesfully!" std::endl;
+		}
+    		try {
+      		connection C("dbname = market user = employee password = test hostaddr = 127.0.0.1 port = 5432");
+      		if (C.is_open()) {
+			std::cout << "Opened database successfully: " << C.dbname() << std::endl;
+      		} else {
+			std::cout << "Can't open database" << std::endl;
+			return 1;
+      		}
+		} catch (const std::exception &e) {
+			std::cout << "DATABASE SETUP FAILED!";
+  			std::cerr << e.what() << std::endl;
+			return 1;
+      		}
+	}
+	return 0;
   }
 };
--- Sections/Postgres/tablesetup.hpp
+++ Sections/Postgres/tablesetup.hpp
@@ -2,50 +2,109 @@
 
 using namespace pqxx;
 
-class GameTableInstall {
+class TableInstallForPostgres {
 public:
-  int GamesTableInstallation(){
+  int TableRecords(){
     std::string sql;
-    // std::string testforexisted;
     try {
-      // testforexisted = "SELECT EXISTS (" \
-      //   "SELECT 1" \
-      //   "FROM information_schema.tables" \
-      //   "WHERE table_schema = 'public'" \
-      //   "AND table_name = 'Games');";
-
       sql = "CREATE TABLE Games("			\
 	"ID SERIAL PRIMARY KEY     NOT NULL,"		\
 	"PRODUCTNAME           CHAR(50)    NOT NULL,"	\
 	"BRANDNAME        CHAR(50)    NOT NULL,"		\
 	"PRICE        DOUBLE     NOT NULL,"			\
 	"PRODUCTYEAR            INT     NOT NULL);";
-  connection C("dbname = market user = employee password = test hostaddr = 127.0.0.1 port = 5432");
-  if (C.is_open()) {
-	    work W(C);
-
-	       W.exec( sql );
-	       W.commit();
-	       std::cout << "Records created successfully" << std::endl;
-         C.disconnect ();
-}
-  // if (C.is_open()) {
-	//     work W(C);
-  //     std::string zek = W.exec( testforexisted );
-  //     if (  zek == 'NULL' ){
-  //       std::cout << "the games table existed!";
-  //       C.disconnect ();
-  //     }else{
-	//        W.exec( sql );
-	//        W.commit();
-	//        std::cout << "Records created successfully" << std::endl;
-  //        C.disconnect ();
-  //      }
-
+	connection C("dbname = market user = employee password = test hostaddr = 127.0.0.1 port = 5432");
+  	if (C.is_open()) {
+		work W(C);
+		W.exec( sql );
+		W.commit();
+		std::cout << "Games Records created successfully" << std::endl;
+		C.disconnect ();
+	}
     } catch (const std::exception &e) {
-      std::cerr << e.what() << std::endl;
-      return 1;
+    	std::cerr << e.what() << std::endl;
+      	return 1;
+    }
+    
+    try {
+      sql = "CREATE TABLE Toys("			\
+	"ID SERIAL PRIMARY KEY     NOT NULL,"		\
+	"PRODUCTNAME           CHAR(50)    NOT NULL,"	\
+	"BRANDNAME        CHAR(50)    NOT NULL,"		\
+	"PRICE        DOUBLE     NOT NULL,"			\
+	"PRODUCTYEAR            INT     NOT NULL);";
+	connection C("dbname = market user = employee password = test hostaddr = 127.0.0.1 port = 5432");
+  	if (C.is_open()) {
+		work W(C);
+		W.exec( sql );
+		W.commit();
+		std::cout << "Toys Records created successfully" << std::endl;
+		C.disconnect ();
+	}
+    } catch (const std::exception &e) {
+    	std::cerr << e.what() << std::endl;
+      	return 1;
+    }
+
+    try {
+      sql = "CREATE TABLE Garden("			\
+	"ID SERIAL PRIMARY KEY     NOT NULL,"		\
+	"PRODUCTNAME           CHAR(50)    NOT NULL,"	\
+	"BRANDNAME        CHAR(50)    NOT NULL,"	\	
+	"SECTION	CHAR(50)	NOT NULL,"	\
+	"PRICE        DOUBLE     NOT NULL);";
+	connection C("dbname = market user = employee password = test hostaddr = 127.0.0.1 port = 5432");
+  	if (C.is_open()) {
+		work W(C);
+		W.exec( sql );
+		W.commit();
+		std::cout << "Garden Records created successfully" << std::endl;
+		C.disconnect ();
+	}
+    } catch (const std::exception &e) {
+    	std::cerr << e.what() << std::endl;
+      	return 1;
+    }
+
+    try {
+      sql = "CREATE TABLE Books("			\
+	"ID SERIAL PRIMARY KEY     NOT NULL,"		\
+	"PRODUCTNAME           CHAR(50)    NOT NULL,"	\
+	"BRANDNAME        CHAR(50)    NOT NULL,"		\
+	"PRICE        DOUBLE     NOT NULL,"			\
+	"ISBN	INT(30)	NOT NULL,"		\
+	"PRODUCTYEAR            INT     NOT NULL);";
+	connection C("dbname = market user = employee password = test hostaddr = 127.0.0.1 port = 5432");
+  	if (C.is_open()) {
+		work W(C);
+		W.exec( sql );
+		W.commit();
+		std::cout << " Books Records created successfully" << std::endl;
+		C.disconnect ();
+	}
+    } catch (const std::exception &e) {
+    	std::cerr << e.what() << std::endl;
+      	return 1;
     }
     return 0;
   }
+
+  int ExistTableTest( string tablename ){
+    try {
+      string sql = "SELECT COUNT(*) FROM information_schema.tables WHERE table_name = '" + tablename  + "';";			\
+	connection C("dbname = market user = employee password = test hostaddr = 127.0.0.1 port = 5432");
+  	if (C.is_open()) {
+		work W(C);
+		W.exec( sql );
+		W.commit();
+		std::cout << " Books Records created successfully" << std::endl;
+		C.disconnect ();
+		return 0;
+	}
+    } catch (const std::exception &e) {
+    	std::cerr << e.what() << std::endl;
+      	return 1;
+    }	
+  }
+
 };
